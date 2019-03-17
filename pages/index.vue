4f3b418bd4602a11599d0dcbc153519e6fa8794f
jim <zhoujun247@foxmail.com>
2019-03-17 15:56:42 +0800

    commit

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      1
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
