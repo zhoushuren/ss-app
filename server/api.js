3263a53a2103823df3160a14be20ee9893fc3501
jim <zhoujun247@foxmail.com>
2019-03-31 13:09:42 +0800

    加入了网页

--- server/api.js
+++ server/api.js
@@ -63,6 +63,8 @@ async function newAccount() {
     method: 'get',
     json: true
   })
+  console.log(portObj)
+  console.log(111111)
   if (portObj && portObj.port) {
     let obj = await rp({
       uri:  host + '/app/account',
