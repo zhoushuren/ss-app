e630d4fed17b123ba85431a63de71aaf0ffde1da
jim <zhoujun247@foxmail.com>
2019-04-04 20:05:26 +0800

    加入了网页

--- pages/notify.vue
+++ pages/notify.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>这是测试的通知页面 你的uuid是： {{ uuid }}</div>
+  <v-container grid-list-md>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md8 align-center justify-center layout text-xs-center>
+        暂无公告
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
