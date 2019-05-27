8b659f9e25d9308ead93e3ab33d426686f080c85
周树人 <zhoujun247@foxmail.com>
2019-05-27 18:14:38 +0800

    加了规则API

--- server/routers.js
+++ server/routers.js
@@ -8,5 +8,6 @@ router.use(bodyParser())
 router.get('/config', api.config)
 router.get('/get_user_info', api.getuserInfo)
 router.post('/user_signup', api.saveUserInfo)
+router.post('/rulesets/detail', api.ruleset)
 
 module.exports = router
